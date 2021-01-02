edfacb41b4303f987084bd3a6cb04f50f62349a2
Nick Provost <nickprovs@hotmail.com>
2021-01-01 23:38:59 -0500

    Some project restructuring

---
+++ server/server.js
@@ -0,0 +1,11 @@
+let express = require("express"); 
+
+const app = express();
+app.use(express.static(__dirname + "/public"));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/public/index.html"));
+});
+
+const port = process.env.PORT ? process.env.PORT : 3000;
+const server = app.listen(port);
+module.exports = server;
